3d2cc0a77810b4df004f95dce22d6cf6992ac051
drralsamyn-lang <drralsamyn@gmail.com>
2025-12-14 23:21:47 +0300

    Add files via upload

---
+++ مخمخ.py
@@ -0,0 +1,15 @@
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_server():
+    port = int(os.environ.get("PORT", 10000))
+    HTTPServer(("", port), Handler).serve_forever()
+
+threading.Thread(target=run_server).start()
